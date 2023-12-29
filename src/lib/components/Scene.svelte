7e898207cbb894ee0ca1757d6e8c52cc5fbc0e28
Rafael-Urei <rafaelss.dev@gmail.com>
2023-12-29 16:40:53 -0300

    Finished the Project

--- src/lib/components/Scene.svelte
+++ src/lib/components/Scene.svelte
@@ -1,60 +1,19 @@
 <script lang="ts">
-  import { T, useFrame } from '@threlte/core'
-
-  let rotation = 0
-  useFrame(() => {
-    rotation += 0.001
-  })
+	import { T, useTask } from '@threlte/core';
+	import { GLTF, OrbitControls } from '@threlte/extras';
+	let rotation = 0;
+	useTask((delta) => {
+		rotation += delta / 4;
+	});
 </script>
 
-<T.Group rotation.y={rotation}>
-  <T.PerspectiveCamera
-    makeDefault
-    position={[-10, 10, 10]}
-    fov={15}
-    on:create={({ ref }) => {
-      ref.lookAt(0, 1, 0)
-    }}
-  />
-</T.Group>
+<T.PerspectiveCamera makeDefault fov={5} position={[10, 40, 20]}>
+	<OrbitControls />
+</T.PerspectiveCamera>
 
-<!-- Floor -->
-<T.Mesh rotation.x={(90 * Math.PI) / 180}>
-  <T.CircleGeometry args={[3, 16]} />
-  <T.MeshBasicMaterial
-    color="#666666"
-    wireframe
-  />
-</T.Mesh>
+<T.AmbientLight color="blue" intensity={4} />
+<T.PointLight intensity={2} position.y={0.6} />
+<T.DirectionalLight color="blue" position.y={-0.6} />
+<T.DirectionalLight position.y={-0.6} />
 
-<T.DirectionalLight
-  intensity={0.8}
-  position.x={5}
-  position.y={10}
-/>
-<T.AmbientLight intensity={0.2} />
-
-<T.Mesh
-  position.y={1.2}
-  position.z={-0.75}
->
-  <T.BoxGeometry />
-  <T.MeshStandardMaterial color="#0059BA" />
-</T.Mesh>
-
-<T.Mesh
-  position={[1.2, 1.5, 0.75]}
-  rotation.x={5}
-  rotation.y={71}
->
-  <T.TorusKnotGeometry args={[0.5, 0.15, 100, 12, 2, 3]} />
-  <T.MeshStandardMaterial color="#F85122" />
-</T.Mesh>
-
-<T.Mesh
-  position={[-1.4, 1.5, 0.75]}
-  rotation={[-5, 128, 10]}
->
-  <T.IcosahedronGeometry />
-  <T.MeshStandardMaterial color="#F8EBCE" />
-</T.Mesh>
+<GLTF url="/models/svelte.glb" rotation.y={rotation} rotation.x={rotation} />
